912e11a72a5d8aedb33c847dce4937810d49a847
pfundaro9 <78462157+pfundaro9@users.noreply.github.com>
2021-04-07 11:39:42 -0400

    Added Reg Form ID's

--- MediForm/src/main/java/sample/regNewPatientController.java
+++ MediForm/src/main/java/sample/regNewPatientController.java
@@ -15,6 +15,46 @@ import java.util.ResourceBundle;
 
 public class regNewPatientController implements Initializable {
 
+
+    @FXML
+    private TextField firstName1;
+
+    @FXML
+    private TextField lastName2;
+
+    @FXML
+    private TextField dateOfBirth1;
+
+    @FXML
+    private TextField address1;
+
+    @FXML
+    private TextField city1;
+
+    @FXML
+    private TextField state1;
+
+    @FXML
+    private TextField phoneNumber1;
+
+    @FXML
+    private TextField insuranceProvider1;
+
+    @FXML
+    private TextField primaryPhysician1;
+
+    @FXML
+    private TextField emergContactName1;
+
+    @FXML
+    private TextField emergPhoneNumber1;
+
+    @FXML
+    private TextField reasonForVisit1;
+
+    @FXML
+    private TextField patientID1;
+
     @FXML
     void toRegHome1(ActionEvent event) throws IOException {
         Parent regHomeParent1 = FXMLLoader.load(getClass().getResource("regHome.fxml"));
