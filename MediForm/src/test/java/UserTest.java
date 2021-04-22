c86df41a1a9482e25832a662eec5eb3c1497b9f0
Nreilly30 <60191822+Nreilly30@users.noreply.github.com>
2021-04-22 15:47:51 -0400

    added test to user

--- MediForm/src/test/java/UserTest.java
+++ MediForm/src/test/java/UserTest.java
@@ -1,19 +1,59 @@
 import org.junit.Test;
+import org.bson.*;
 
 import static org.junit.Assert.*;
 
 public class UserTest {
 
     @Test
-    public void TestUser() throws Exception {
+    public void TestReg() throws Throwable {
         // user is the contructor of User class.
-        User user = new User("Nic", "Nreilly", "Nic", 2);
+        User user = new User("Registration", "RegUser", "RegPass01", 1);
 
         //Assert equals. First value is expected, what you thing the value should be.
         //Second value is retrieving what the value is in User.
-        assertEquals("Nic", user.getName());
-        assertEquals("Nreilly", user.getUsername());
-        assertEquals("Nic", user.getPassword());
+        assertEquals("Registration", user.getName());
+        assertEquals("RegUser", user.getUsername());
+        assertEquals("RegPass01", user.getPassword());
+        assertEquals(1, user.getRoleID());
+    }
+
+    @Test
+    public void TestNurse() throws Throwable {
+        // user is the contructor of User class.
+        User user = new User("Nurse", "NurseUser", "NursePass01", 2);
+
+        //Assert equals. First value is expected, what you thing the value should be.
+        //Second value is retrieving what the value is in User.
+        assertEquals("Nurse", user.getName());
+        assertEquals("NurseUser", user.getUsername());
+        assertEquals("NursePass01", user.getPassword());
         assertEquals(2, user.getRoleID());
+    }
+
+    @Test
+    public void TestPhy() throws Throwable {
+        // user is the contructor of User class.
+        User user = new User("Physician", "PhyUser", "PhyPass01", 3);
+
+        //Assert equals. First value is expected, what you thing the value should be.
+        //Second value is retrieving what the value is in User.
+        assertEquals("Physician", user.getName());
+        assertEquals("PhyUser", user.getUsername());
+        assertEquals("PhyPass01", user.getPassword());
+        assertEquals(3, user.getRoleID());
+    }
+
+    @Test
+    public void TestBill() throws Throwable {
+        // user is the contructor of User class.
+        User user = new User("Billing", "BillUser", "BillPass01", 4);
+
+        //Assert equals. First value is expected, what you thing the value should be.
+        //Second value is retrieving what the value is in User.
+        assertEquals("Billing", user.getName());
+        assertEquals("BillUser", user.getUsername());
+        assertEquals("BillPass01", user.getPassword());
+        assertEquals(4, user.getRoleID());
     }
 }
